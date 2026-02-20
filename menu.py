84f742a94842c92eeb3f207d17418f5e49afe7ab
hacker3983 <mrgamer19554@gmail.com>
2026-02-20 15:23:20 -0500

    Implemented input validation, color system and switch account feature

--- menu.py
+++ menu.py
@@ -2,7 +2,14 @@ from auth import Auth
 from manager import Manager
 import getpass
 
-banner = """\u001b[33m
+white = "\033[38;2;255;255;255m"
+red = "\033[31m"
+bold = "\033[1m"
+green = "\033[38;2;;255;m"
+yellow = "\033[38;2;255;255;m"
+reset = "\033[0m"
+
+banner = f"""{yellow}
 ███████╗██╗     ███████╗██╗  ██╗██████╗  █████╗ ███████╗███████╗
 ██╔════╝██║     ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝
 █████╗  ██║     █████╗   ╚███╔╝ ██████╔╝███████║███████╗███████╗
@@ -16,7 +23,7 @@ banner = """\u001b[33m
 ██║╚██╔╝██║██╔══██║██║╚██╗██║██╔══██║██║   ██║██╔══╝  ██╔══██╗  
 ██║ ╚═╝ ██║██║  ██║██║ ╚████║██║  ██║╚██████╔╝███████╗██║  ██║  
 ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝╚═╝  ╚═╝  
-\u001b[0m"""
+{reset}"""
 
 class Menu:
     def __init__(self):
@@ -28,6 +35,7 @@ class Menu:
                 "Add a Credential",
                 "View Credentials",
                 "Delete Credentials",
+                "Switch Account",
                 "Logout"
         ]
 
@@ -47,14 +55,21 @@ class Menu:
         self.auth.write_data()
 
     def print_startup_menu(self):
-        print("version:", self.version)
+        print(f"{yellow}version{reset}:{bold}{red} {self.version}\n{reset}")
         for i, option in enumerate(self.startup_options):
-            print(f"[{i+1}] {option}")
+            print(f"{green}[{yellow}{i+1}{green}]", end="")
+            print(f"{white} {option}{reset}")
+        print()
 
     def startup_menu(self):
         print(banner)
         self.print_startup_menu()
-        option = int(input(f"Select an option (1-{len(self.startup_options)}): "))
+        option = None
+        try:
+            option = int(input(f"{white}Select an option ({bold}{red}1-{len(self.startup_options)}{white})> "))
+        except:
+            print("The option that you gave is invalid please try again!")
+            return
         if option == 1:
             self.auth.signup()
         elif option == 2:
@@ -63,20 +78,29 @@ class Menu:
             self.manager.master_password = self.auth.master_password
         elif option == 3:
             self.quit = True
+        else:
+            print("The option that you gave is invalid please try again!")
 
     def logout(self):
         self.auth.logged_in = False
         print(f"Logged out of the user {self.auth.username}!")
 
     def print_vaultmenu(self):
+        print(banner)
         print(f"Welcome, {self.auth.username}!")
         for i, option in enumerate(self.vault_options):
-                print(f"[{i+1}] {option}")
+            print(f"{green}[{yellow}{i+1}{green}]{white} {option}{reset}")
+        print()
 
     def vault_menu(self):
         while True:
             self.print_vaultmenu()
-            option = int(input(f"Select an option (1-{len(self.vault_options)})> "))
+            option = input(f"{white}Select an option ({bold}{red}1-{len(self.vault_options)}{white})> ")
+            try:
+                option = int(option)
+            except:
+                print("The option you entered is invalid please try again...")
+                continue
             if option == 1:
                 print(self.add_credential())
             elif option == 2:
@@ -85,15 +109,36 @@ class Menu:
                 self.delete_credentials()
             elif option == 4:
                 self.logout()
+                self.auth.login()
+                print(f"{bold}{white}Successfully switched into account!")
+            elif option == 5:
+                self.logout()
                 break
 
     def ask_credential(self):
         print("Please provide your credential details below...")
-        platform_name = input("Enter the platform (Ex: Gmail): ")
-        username = input("Enter the username: ")
-        password = getpass.getpass("Enter the password: ")
-        if password != getpass.getpass("confirm the password: "):
-            return None
+        platform_name = None
+        username = None
+        password = None
+        while True:
+            platform_name = input("Enter the platform (Ex: Gmail): ")
+            if input("Enter the platform name again: ") == platform_name:
+                break
+            print("The platform name you have provided does not match...")
+            print("Please try again...")
+
+        while True:
+            username = input("Enter the username: ")
+            if input("Confirm you username: ") == username:
+                break
+            print("The username you entered does not match...")
+            print("Please try again...")
+        while True:
+            password = getpass.getpass("Enter the password: ")
+            if password == getpass.getpass("confirm the password: "):
+                break
+            print("The password that you have entered does not match...")
+            print("Please try again...")
         return (platform_name, username, password)
 
     def add_credential(self):
@@ -108,12 +153,12 @@ class Menu:
     def print_platforms(self):
         platforms = self.manager.get_platforms()
         for option, platform_name in enumerate(platforms):
-            print(f"[{option+1}] {platform_name}")
+            print(f"{bold}{green}[{yellow}{option+1}{green}]{white} {platform_name}{reset}")
 
     def print_credentials(self, platform_name):
         platform = self.manager.get_platform(platform_name)
         for i, username in enumerate(platform):
-            print(f"[{i+1}] {username}")
+            print(f"{bold}{green}[{yellow}{i+1}{green}]{white} {username}{reset}")
 
     def view_credentials(self):
         platforms = self.manager.get_platforms()
@@ -121,19 +166,46 @@ class Menu:
             print("There is no credential for the current user...")
             return None
         platform_count = len(platforms)
-        self.print_platforms()
-        platform_id = int(input(f"Select a platform (1-{platform_count}): ")) - 1
-
-        platform_name = self.manager.get_platform_name(platform_id)
-        credentials = list(self.manager.get_platform(platform_name))
-        credential_count = len(credentials)
-        self.print_credentials(platform_name)
-        credential_id = int(input(f"Select a credential (1-{credential_count}): ")) - 1
-
-        credential_username = credentials[credential_id]
-        credential_password = self.manager.retrieve_credential(platform_name, credential_id)
-        print("Your credential is:")
-        print(f"{credential_username} -> {credential_password}")
+        while True:
+            self.print_platforms()
+            print("Type back to go back to the main menu...")
+            platform_id = input(f"{white}Select a platform ({bold}{red}1-{platform_count}{white}): ")
+            if platform_id == "back":
+                break
+            try:
+                platform_id = int(platform_id) - 1
+            except:
+                print("The platform id provided is invalid...")
+                print("Please try again...")
+                continue
+            if platform_id >= platform_count or platform_id < 0:
+                print("The platform id provided is invalid...")
+                print("Please try again..")
+                continue
+            print(platform_id)
+            platform_name = self.manager.get_platform_name(platform_id)
+            credentials = list(self.manager.get_platform(platform_name))
+            credential_count = len(credentials)
+            while True:
+                self.print_credentials(platform_name)
+                print("Type back to go back to selecting a platform option...")
+                credential_id = input(f"{white}Select a credential ({bold}{red}1-{credential_count}{white}): ")
+                if credential_id == "back":
+                    break
+                try:
+                    credential_id = int(credential_id) - 1
+                except:
+                    print("The credential id you entered is not valid...")
+                    print("Please try again...")
+                    continue
+                if credential_id >= credential_count or credential_id < 0:
+                    print("The credential id you entered is invalid...")
+                    print("Please try again...")
+                    continue
+                credential_username = credentials[credential_id]
+                credential_password = self.manager.retrieve_credential(platform_name, credential_id)
+                print("Your credential is:")
+                print(f"{credential_username} -> {credential_password}")
         return platforms
 
     def delete_credentials(self):
@@ -142,39 +214,50 @@ class Menu:
             print("There is no credential for the current user...")
             return None
         platform_count = len(platforms)
-        self.print_platforms()
-        platform_id = int(input(f"Select a platform (1-{platform_count}): ")) - 1
+        while True:
+            self.print_platforms()
+            print("Type back to go back to the main menu")
+            platform_id = input(f"{white}Select a platform ({bold}{red}1-{platform_count}{white}): ")
+            if platform_id == "back":
+                return None
+            try:
+                platform_id = int(platform_id) - 1
+            except:
+                print("The platform id entered is invalid...")
+                print("Please try again...")
+                continue
+            if platform_id >= platform_count or platform_id < 0:
+                print("The platform id is invalid...")
+                print("Please try again...")
+                continue
+            platform_name = self.manager.get_platform_name(platform_id)
+            credentials = list(self.manager.get_platform(platform_name))
+            credential_count = len(credentials)
+            while True:
+                self.print_credentials(platform_name)
+                print("Type back to go back to platform selection")
+                credential_id = input(f"{white}Select a credential ({bold}{red}1-{credential_count}{white}): ")
+                if credential_id == "back":
+                    break
+                try:
+                    credential_id = int(credential_id) - 1
+                except:
+                    print("The credential id is invalid...")
+                    print("Please try again...")
+                    continue
+                if credential_id >= credential_count or credential_id < 0:
+                    print("The credential id is invalid...")
+                    print("Please try again...")
+                    continue
 
-        platform_name = self.manager.get_platform_name(platform_id)
-        credentials = list(self.manager.get_platform(platform_name))
-        credential_count = len(credentials)
-        self.print_credentials(platform_name)
-        credential_id = int(input(f"Select a credential (1-{credential_count}): ")) - 1
-
-        credential_username = credentials[credential_id]
-        removed = self.manager.remove_credential(platform_name, credential_id)
-        if not removed:
-            print("Failed to remove the credential...")
-            return removed
-        print("Successfully removed the credential!")
-        print(f"Removed: {credential_username}")
-        return removed
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+                credential_username = credentials[credential_id]
+                removed = self.manager.remove_credential(platform_name, credential_id)
+                if not removed:
+                    print("Failed to remove the credential...")
+                    continue
+                print("Successfully removed the credential!")
+                print(f"Removed: {credential_username}")
+                credentials = self.manager.get_platform(platform_name)
+                if len(credentials) == 0:
+                    break
 
